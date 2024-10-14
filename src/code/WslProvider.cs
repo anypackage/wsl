2ed74c1dd077c4c7a5c985a2979b7bd80c65616f
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-10-14 11:59:38 -0500

    Add Find-Package

--- src/code/WslProvider.cs
+++ src/code/WslProvider.cs
@@ -9,9 +9,52 @@ using System.Text.RegularExpressions;
 namespace AnyPackage.Provider.Wsl;
 
 [PackageProvider("Wsl")]
-public class WslProvider : PackageProvider, IGetPackage
+public class WslProvider : PackageProvider, IFindPackage, IGetPackage
 {
+    private const string _findRegex = @"^(?<name>\S+)\s{2,}(?<friendlyName>.+)$";
     private const string _getRegex = @"^(?<default>[\*]?)\s+(?<name>\S+)\s+(?<state>\S+)\s+(?<version>\d+)$";
+
+    public void FindPackage(PackageRequest request)
+    {
+        if (request.IsVersionFiltered)
+        {
+            return;
+        }
+
+        using var process = new Process();
+        process.StartInfo.Arguments = "--list --online";
+        process.StartInfo.FileName = "wsl";
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.StandardOutputEncoding = Encoding.Unicode;
+        process.Start();
+        using var reader = process.StandardOutput;
+
+        string line;
+
+        while ((line = reader.ReadLine()) is not null)
+        {
+            var match = Regex.Match(line, _findRegex);
+
+            if (match.Success && match.Groups["name"].Value != "NAME")
+            {
+                var metadata = new Dictionary<string, object?>
+                {
+                    { "FriendlyName", match.Groups["friendlyName"].Value }
+                };
+
+                var package = new PackageInfo(match.Groups["name"].Value,
+                                              version: null,
+                                              source: null,
+                                              description: "",
+                                              dependencies: null,
+                                              metadata,
+                                              ProviderInfo);
+
+                request.WritePackage(package);
+            }
+        }
+    }
 
     public void GetPackage(PackageRequest request)
     {
