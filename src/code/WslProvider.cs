c1cf492503b76ae2dd966bd7768b7a8dc7a09d83
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-10-14 18:07:34 -0500

    Add GetStartInfo method

--- src/code/WslProvider.cs
+++ src/code/WslProvider.cs
@@ -23,16 +23,8 @@ public class WslProvider : PackageProvider, IFindPackage, IGetPackage
             return;
         }
 
-        var args = "--list --online";
-        var message = string.Format(_running, _fileName, args);
-        request.WriteVerbose(message);
-
         using var process = new Process();
-        process.StartInfo.Arguments = args;
-        process.StartInfo.FileName = _fileName;
-        process.StartInfo.CreateNoWindow = true;
-        process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.StandardOutputEncoding = Encoding.Unicode;
+        process.StartInfo = GetStartInfo("--list --online", request);
         process.Start();
         using var reader = process.StandardOutput;
 
@@ -60,16 +52,8 @@ public class WslProvider : PackageProvider, IFindPackage, IGetPackage
 
     public void GetPackage(PackageRequest request)
     {
-        var args = "--list --online";
-        var message = string.Format(_running, _fileName, args);
-        request.WriteVerbose(message);
-
         using var process = new Process();
-        process.StartInfo.Arguments = args;
-        process.StartInfo.FileName = _fileName;
-        process.StartInfo.CreateNoWindow = true;
-        process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.StandardOutputEncoding = Encoding.Unicode;
+        process.StartInfo = GetStartInfo("--list --verbose", request);
         process.Start();
         using var reader = process.StandardOutput;
 
@@ -101,5 +85,20 @@ public class WslProvider : PackageProvider, IFindPackage, IGetPackage
                 }
             }
         }
+    }
+
+    private static ProcessStartInfo GetStartInfo(string args, PackageRequest request)
+    {
+        var message = string.Format(_running, _fileName, args);
+        request.WriteVerbose(message);
+
+        var startInfo = new ProcessStartInfo(_fileName, args)
+        {
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            StandardOutputEncoding = Encoding.Unicode
+        };
+
+        return startInfo;
     }
 }
